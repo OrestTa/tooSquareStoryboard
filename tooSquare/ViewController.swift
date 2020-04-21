0b5a669d43856e5efcf1c6610e5ad3d67984a19a
Orest Tarasiuk <orest@tarasiuk.me>
2020-04-21 12:43:32 +0200

    Initial Commit

---
+++ tooSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tooSquare
+//
+//  Created by Orest Tarasiuk on 21/4/20.
+//  Copyright © 2020 orest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
